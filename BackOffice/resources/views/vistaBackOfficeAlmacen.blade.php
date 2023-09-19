b0ee382ff26d44a841f1effd47e1110aa25469b6
AlejandroJMP24 <alejandromachado85@gmail.com>
2023-09-19 14:25:40 -0300

    almacenes pasado a php

--- BackOffice/resources/views/vistaBackOfficeAlmacen.blade.php
+++ BackOffice/resources/views/vistaBackOfficeAlmacen.blade.php
@@ -5,23 +5,24 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>BackOffice:Almacenes</title>
      <link rel="stylesheet" href="{{ asset('css/style.css') }}"> 
-     <script src="{{asset('js/funciones.js')}}"> </script>
-     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
      <meta name="csrf-token" content="{{ csrf_token() }}">
   </head>
   <body>
     @include('header')
      <div class="barraDeNavegacion">
-       <div class="item" onclick="redireccionar('{{route('backoffice')}}')"> Menu Principal</div>
-       <div class="itemSeleccionado" onclick="redireccionar('{{route('backoffice.almacen')}}')">Almacenes</div>
-       <div class="item" onclick="redireccionar('{{route('backoffice.camiones')}}')"> Camiones</div>
-    <div class="item" onclick="redireccionar('{{route('backoffice.paquete')}}')"> Paquetes</div>
-    <div class="item" onclick="redireccionar('{{route('backoffice.producto')}}')"> Productos</div>
-    <div class="item" onclick="redireccionar('{{route('backoffice.lote')}}')"> Lotes</div>
+     <a href="{{ route('backoffice') }}" class="item">Menu Principal</a>
+     <a href="{{ route('backoffice.almacen') }}" class="itemSeleccionado">Almacenes</a>
+     <a href="{{ route('backoffice.camiones') }}" class="item">Camiones</a>
+     <a href="{{ route('backoffice.paquete') }}" class="item">Paquetes</a>
+     <a href="{{ route('backoffice.producto') }}" class="item">Productos</a>
+     <a href="{{ route('backoffice.lote') }}" class="item">Lotes</a>
+
      </div>
      <div class="container">
        <div class="cuerpo">
-        <div id="contenedorTabla"></div>
+        <div id="contenedorTabla">
+        <x-tabla-almacenes-component/>
+        </div>
        </div>
        <div> 
         <button onclick="redireccionar('{{route('almacen.lugarEntrega')}}')">Luares de entrega-></button>
@@ -51,10 +52,11 @@
           <form action="{{route('almacen.cargarDatos')}}" method="GET">
             <button type="submit" name="cargar">Cargar Datos</button>
           </form>
-          <button type="button"  name="cargarTabla" id="cargarTabla" onclick="crearTabla(1, {{ json_encode(session('almacenes', [])) }})">Cargar Tabla</button>
        </div>
      </div>
    </div>
+</div>
+
   </body>
   @include('footer')
 </html>
