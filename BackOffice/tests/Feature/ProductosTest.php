1844b334fe2cf15568331284829f2962db262e64
Matias Meneses <mat.meneses47@gmail.com>
2023-09-21 18:11:32 -0300

    Creacion y finalización de pruebas unitarias en Telefonos, bonitas y preciosas

---
+++ BackOffice/tests/Feature/ProductosTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductosTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    // public function test_modificarUnProducto() {
+    //   $estructura = [
+    //    "id",
+    //    "nombre",
+    //    "precio",
+    //    "stock",
+    //    "id_moneda"
+    //   ];
+       
+    //   $response = $this->post('/productos', [
+     //   "nombre" => "producto modifica2",
+    //    "precio" => "2",
+     //   "stock" => "1",
+    //    "id_moneda" => "2",
+     //  ]);
+    //   $response ->assertStatus(200);
+
+       
+
+   // }
+}
