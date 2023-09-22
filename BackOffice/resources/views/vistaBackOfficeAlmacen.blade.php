6835d26cb5b0e80f32d2dfc6660aaaf49c62991f
AlejandroJMP24 <alejandromachado85@gmail.com>
2023-09-21 21:58:11 -0300

    limpieza codigo

--- BackOffice/resources/views/vistaBackOfficeAlmacen.blade.php
+++ BackOffice/resources/views/vistaBackOfficeAlmacen.blade.php
@@ -37,7 +37,8 @@
             <input type="checkbox" name="cbxRecuperar" id="cbxRecuperar" onclick="comprobarCbxRecuperar()">Recuperar </input>
             <div class="contenedorDatos">
             <div class="campo">
-            <x-select-lugares-entrega-component/>
+              <input type="text" name="direccion" id="direccion" maxlength="100">
+              <label for="direccion">Direccion</label>
             </div>
             <div class="campo">
             <input type="text" id="latitud" name="latitud" onkeydown="filtro(event)" 
