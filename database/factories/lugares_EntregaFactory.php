c7d2a52c1a8037e016e387d121245018f8a805b4
Matias Meneses <mat.meneses47@gmail.com>
2023-11-04 13:58:56 -0300

    16

--- database/factories/lugares_EntregaFactory.php
+++ database/factories/lugares_EntregaFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class lugares_EntregaFactory extends Factory
+class Lugares_EntregaFactory extends Factory
 {
     /**
      * Define the model's default state.
