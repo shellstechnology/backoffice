acad27263d4a86c7e39a098312ec866113d7e8da
Matias Meneses <mat.meneses47@gmail.com>
2023-11-04 14:24:09 -0300

    22

---
+++ database/factories/Mail_UsuariosFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Mail_UsuariosFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
