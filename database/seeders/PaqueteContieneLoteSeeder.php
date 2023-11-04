12f11378f8873e4f1a3832fb83345ce1c47b8b94
Matias Meneses <mat.meneses47@gmail.com>
2023-11-04 14:06:41 -0300

    17?

--- database/seeders/PaqueteContieneLoteSeeder.php
+++ database/seeders/PaqueteContieneLoteSeeder.php
@@ -21,42 +21,42 @@ class PaqueteContieneLoteSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\paquete_Contiene_Lote::factory(1)->create([
+        \App\Models\paquete_contiene_lote::factory(1)->create([
             "id_paquete" => "10",
             "id_lote" => "20",
             "id_almacen" => "47",
         ]);
-        \App\Models\paquete_Contiene_Lote::factory(1)->create([
+        \App\Models\paquete_contiene_lote::factory(1)->create([
             "id_paquete" => "47",
             "id_lote" => "47",
             "id_almacen" => "47",
         ]);
-        \App\Models\paquete_Contiene_Lote::factory(1)->create([
+        \App\Models\paquete_contiene_lote::factory(1)->create([
             "id_paquete" => "42",
             "id_lote" => "42",
             "id_almacen" => "47",
         ]);
-        \App\Models\paquete_Contiene_Lote::factory(1)->create([
+        \App\Models\paquete_contiene_lote::factory(1)->create([
             "id_paquete" => "74",
             "id_lote" => "74",
             "id_almacen" => "74",
         ]);
-        \App\Models\paquete_Contiene_Lote::factory(1)->create([
+        \App\Models\paquete_contiene_lote::factory(1)->create([
             "id_paquete" => "100",
             "id_lote" => "100",
             "id_almacen" => "47",
         ]);
-        \App\Models\paquete_Contiene_Lote::factory(1)->create([
+        \App\Models\paquete_contiene_lote::factory(1)->create([
             "id_paquete" => "101",
             "id_lote" => "100",
             "id_almacen" => "47",
         ]);
-        \App\Models\paquete_Contiene_Lote::factory(1)->create([
+        \App\Models\ppaquete_contiene_lote::factory(1)->create([
             "id_paquete" => "102",
             "id_lote" => "100",
             "id_almacen" => "47",
         ]);
-        \App\Models\paquete_Contiene_Lote::factory(1)->create([
+        \App\Models\paquete_contiene_lote::factory(1)->create([
             "id_paquete" => "103",
             "id_lote" => "100",
             "id_almacen" => "47",
