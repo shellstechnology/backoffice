79d67f778a8bde55232cdf5a8629b0b6122f92c1
Matias Meneses <mat.meneses47@gmail.com>
2023-11-07 15:22:44 -0300

    cambios para autenticacion personal de la aplicacion sin tocar api autenticacion terminada y funcional

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -6,15 +6,28 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Http;
 use Session;
+use Illuminate\Support\Facades\Auth;
 
 
 class loginController extends Controller
 {
-    public function iniciarSesion(Request $request){
-        $response=Http::post('http://127.0.0.1:8003/api/v1/login', [
-            'name' => $request->input('name'),
-            'password' => $request->input('password'),
-        ]);
-        dd($response->json());
-    }
+   // public function iniciarSesion(Request $request){
+    //    $response=Http::post('http://127.0.0.1:8003/api/v1/login', [
+    //        'name' => $request->input('name'),
+     //       'password' => $request->input('password'),
+     //   ]);
+    //    dd($response->json());
+   // }
+   
+   public function iniciarSesion(Request $request){
+    $credentials = $request->only('name', 'password');
+    if (Auth::attempt($credentials)) 
+        return redirect("/");
+    return redirect("/login")->with("failed",true);
+}
+
+public function Logout(Request $request){
+    Auth::logout();
+    return redirect("/login")->with("logout",true);
+}
 }
--- app/Http/Middleware/Autenticacion.php
+++ app/Http/Middleware/Autenticacion.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
 
 class Autenticacion
 {
@@ -15,15 +16,22 @@ class Autenticacion
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
-    {
-        $tokenHeader = [ "Authorization" => $request -> header("Authorization")];
+    //public function handle(Request $request, Closure $next)
+//{
+//$tokenHeader = [ "Authorization" => $request -> header("Authorization")];
 
-        $response = Http::withHeaders($tokenHeader) -> get("http://localhost:8003/api/v1/validate");
-        if($response -> successful())
-            return $next($request);
-        return response(["message" => "No autorizado"], 403);
+  //      $response = Http::withHeaders($tokenHeader) -> get("http://localhost:8003/api/v1/validate");
+//if($response -> successful())
+  //          return $next($request);
+   //     return response(["message" => "No autorizado"], 403);
 
         
-    }
+  //  }
+
+  public function handle(Request $request, Closure $next)
+  {
+      if(Auth::check())
+          return $next($request);
+      return redirect("/login");
+  }
 }
