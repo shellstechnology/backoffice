d7621aade255dd45bccf12451dd23407735fa4f1
Matias Meneses <mat.meneses47@gmail.com>
2023-11-13 20:58:41 -0300

    llamado a los seeders para la creacion de usuarios en actions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,42 @@ class DatabaseSeeder extends Seeder
         $this->call(Estados_CSeeder::class);
         $this->call(CamionesSeeder::class);
         $this->call(camion_lleva_loteSeeder::class);
+    
+            \App\Models\User::factory(1)->create([
+                "name" => "usuario",
+                "email" => "usuario@usuario"
+            ]);
+            \App\Models\User::factory(1)->create([
+                "id" => "10",
+                "name"=>"toreto",
+            ]);
+            \App\Models\User::factory(1)->create([
+                "id" => "20",
+                "name"=>"choferpreba",
+            ]);
+            \App\Models\User::factory(1)->create([
+                "id" => "47",
+                "name"=>"usuario a listar",
+            ]);
+            \App\Models\User::factory(1)->create([
+                "id" => "42",
+                "name"=>"usuario a modificar",
+            ]);
+            \App\Models\User::factory(1)->create([
+                "id" => "74",
+                "name"=>"usuario a eliminar",
+            ]);
+            \App\Models\User::factory(1)->create([
+                "name"=>"admin",
+            ]);
+            \App\Models\User::factory(1)->create([
+                "name"=>"chofer",
+            ]);
+            \App\Models\User::factory(1)->create([
+                "name"=>"almacenero",
+            ]);
+        
+
         $this->call(telefonos_usuariosSeeder::class);
         $this->call(ChoferesSeeder::class);
         $this->call(AdministradoresSeeder::class);
