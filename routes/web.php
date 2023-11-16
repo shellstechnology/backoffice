58b6c00aca055db425946b59a8087bc841138bd2
AlejandroJMP24 <alejandromachado85@gmail.com>
2023-11-15 23:16:11 -0300

    prueba actions

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\loginController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register web routes for your aplication. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
